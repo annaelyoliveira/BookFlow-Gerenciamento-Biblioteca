b522dfc631db0de0cb31f165eb28e9fda700e8b0
Annaely Oliveira <annaely691@gmail.com>
2025-07-17 15:22:36 -0300

    Correção na classe Usuario para incluir a senha.

--- src/main/java/biblioteca.model/Usuario.java
+++ src/main/java/biblioteca.model/Usuario.java
@@ -7,7 +7,63 @@ public class Usuario {
     private String tipoUsuario;
     private String telefone;
     private String email;
+    private String senha;
 
-    
+    public Usuario(String nome, int matricula, String tipoUsuario, String telefone, String email, String senha) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.tipoUsuario = tipoUsuario;
+        this.telefone = telefone;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
 
